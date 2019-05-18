7c37c01f12c4bc4381bdb8d8488e523f262f565b
Wizard V valayathil <nevinvalayathil@gmail.com>
2019-05-18 16:12:32 -0500

    intial commit

---
+++ CoreAPI1/CoreAPI1.Services/Services/UserService.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using CoreAPI1.API.Common.Settings;
+using CoreAPI1.Services.Contracts;
+using CoreAPI1.Services.Model;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace CoreAPI1.Services
+{
+    public class UserService : IUserService
+    {
+        private AppSettings _settings;
+        private readonly IMapper _mapper;
+
+        public UserService(IOptions<AppSettings> settings, IMapper mapper)
+        {
+            _settings = settings?.Value;
+            _mapper = mapper;
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            return user;
+        }
+
+        public async Task<bool> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
